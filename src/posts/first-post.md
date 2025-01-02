64be079fd4aa385c75473b8edded59bb8f963893
fantafi <phantai.dd@gmail.com>
2025-01-02 23:31:27 +0700

    update main page

---
+++ src/posts/first-post.md
@@ -0,0 +1,19 @@
+---
+title: "First Post"
+description: "This is my first post!"
+date: "2025-01-01"
+categories:
+  - blog
+  - tutorial
+published: true
+---
+
+## Contents
+
+## Intro
+
+This is my first post!
+
+## Let's get started
+
+Lorem ipsum dolor sit amet...
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<blockquote class="text-center text-2xl font-semibold italic text-slate-900">
+	When you look
+	<span class="relative">
+		<span class="absolute -inset-1 block -skew-y-3 bg-pink-500" aria-hidden="true"></span>
+		<span class="relative text-white">annoyed</span>
+	</span>
+	all the time, people think that you're busy.
+</blockquote>
